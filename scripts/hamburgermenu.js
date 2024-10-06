71dc676d3ac60839743d5975818a53afe4169ce9
cchardy <pra90003@byui.edu>
2024-10-05 19:26:39 -0700

    hamburger menu

---
+++ scripts/hamburgermenu.js
@@ -0,0 +1,23 @@
+function detectMob() {
+    const toMatch = [
+        /Android/i,
+        /webOS/i,
+        /iPhone/i,
+        /iPad/i,
+        /iPod/i,
+        /BlackBerry/i,
+        /Windows Phone/i
+    ];
+    
+    return toMatch.some((toMatchItem) => {
+        return navigator.userAgent.match(toMatchItem);
+    });
+}
+
+
+if (detectMob()) {
+
+}
+else {
+    document.getElementById("toggleMenu").style.display = "none";
+}
