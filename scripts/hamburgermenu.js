db83a5d21302e1699e54f607b74867f35cd4ad50
cchardy <pra90003@byui.edu>
2025-01-30 14:30:28 -0800

    toggle

--- scripts/hamburgermenu.js
+++ scripts/hamburgermenu.js
@@ -5,3 +5,26 @@ hamButton.addEventListener('click', () => {
     navigation.classList.toggle('show');
     hamButton.classList.toggle('open');
 });
+function detectMob() {
+    const toMatch = [
+        /Android/i,
+        /webOS/i,
+        /iPhone/i,
+        /iPad/i,
+        /iPod/i,
+        /BlackBerry/i,
+        /Windows Phone/i
+    ];
+
+    return toMatch.some((toMatchItem) => {
+        return navigator.userAgent.match(toMatchItem);
+    });
+}
+
+
+if (detectMob()) {
+
+}
+else {
+    document.getElementById("toggleMenu").style.display = "none";
+}
