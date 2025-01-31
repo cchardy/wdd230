c58dbb050299af897b10c1f61bbd4bddd019bfc6
cchardy <pra90003@byui.edu>
2025-01-30 18:38:25 -0800

    last date modified

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,13 @@
 
-const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
 
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+
+let year = document.querySelector("#date");
+year.innerHTML = `&copy${currentYear}`;
+
+let lastModifiedDate = document.querySelector("#lastModified");
+lastModifiedDate.textContent = new Date(document.lastModified);
 
