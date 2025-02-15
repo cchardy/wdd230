99363236c211bdf0f8b37ba26c4d75e6e75fae1c
cchardy <pra90003@byui.edu>
2025-02-15 11:52:46 -0800

    hamburger menu

--- chamber/scripts/hamburgermenu.js
+++ chamber/scripts/hamburgermenu.js
@@ -1,8 +1,9 @@
 // hamburger menu 
-let menuButton = document.querySelector("#menu-button");
-let navList = document.querySelector("nav ul")
-menuButton.addEventListener('click', () => {
-    menuButton.classList.toggle('open')
-    navList.classList.toggle('open')
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('nav');
 
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+    navigation.setAttribute('aria-hidden', navigation.classList.contains('open') ? 'false' : 'true');
 });
