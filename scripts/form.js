ad903a30b2e74833fd4327bae23410537ab9a7e5
cchardy <pra90003@byui.edu>
2025-02-27 15:29:04 -0800

    form build

---
+++ scripts/form.js
@@ -0,0 +1,67 @@
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("r");
+
+// RANGE event listener
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
+
+let confirmItem = document.querySelector("#confirm")
+
+confirmItem.addEventListener("focusout", () => {
+    let passwordItem = document.querySelector("#password")
+    if (confirmItem.value != passwordItem.value) {
+        document.querySelector("#message").innerText = "Passwords don't match!"
+        passwordItem.focus()
+        confirmItem.value = ""
+        confirmItem.focus()
+    }
+    else {
+        document.querySelector("#message").innerText = ""
+    }
+})
+//password confirmation
+const pwd1 = document.querySelector("#passord");
+const pwd2 = document.querySelector("#passord2");
+const message = document.querySelector("#formmessage");
+
+pwd2.addEventListener("focusout", checkSame);
+
+// This should be refactored.
+function checkSame() {
+    if (pwd1.value !== pwd2.value) {
+        message.textContent = "❗Key Phrases DO NOT MATCH!";
+        message.style.visibility = "show";
+        pwd2.style.backgroundColor = "#fff0f3";
+        pwd2.value = "";
+        pwd2.focus();
+    } else {
+        message.style.display = "none";
+        pwd2.style.backgroundColor = "#fff";
+        pwd2.style.color = "#000";
+    }
+}
+
+// Required email
+document.addEventListener("DOMContentLoaded", function () {
+    // Function to handle form submission
+    function handleSubmit(event) {
+        // Prevent default form submission
+        event.preventDefault();
+
+        // Access form elements
+        let formt = event.target;
+        let formData = new FormData(formt);
+
+        // Display form element values
+        for (let pair of formData.entries()) {
+            console.log(pair[0] + ": " + pair[1]);
+        }
+    }
+
+    const form = document.querySelector("form");
+    form.addEventListener("submit", handleSubmit);
+});
