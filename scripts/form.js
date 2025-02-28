54733d14d8a6893a8afb10e7ea3859dcabeed633
cchardy <pra90003@byui.edu>
2025-02-27 18:41:10 -0800

    errors

--- scripts/form.js
+++ scripts/form.js
@@ -9,20 +9,7 @@ function displayRatingValue() {
     rangevalue.innerHTML = range.value;
 }
 
-let confirmItem = document.querySelector("#confirm")
 
-confirmItem.addEventListener("focusout", () => {
-    let passwordItem = document.querySelector("#password")
-    if (confirmItem.value != passwordItem.value) {
-        document.querySelector("#message").innerText = "Passwords don't match!"
-        passwordItem.focus()
-        confirmItem.value = ""
-        confirmItem.focus()
-    }
-    else {
-        document.querySelector("#message").innerText = ""
-    }
-})
 //password confirmation
 const pwd1 = document.querySelector("#password");
 const pwd2 = document.querySelector("#password2");
@@ -65,3 +52,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const form = document.querySelector("form");
     form.addEventListener("submit", handleSubmit);
 });
+
+
+//let confirmItem = document.querySelector("#confirm")
+
+//confirmItem.addEventListener("focusout", () => {
+//    let passwordItem = document.querySelector("#password")
+//    if (confirmItem.value != passwordItem.value) {
+//        document.querySelector("#message").innerText = "Passwords don't match!"
+//        passwordItem.focus()
+//        confirmItem.value = ""
+//        confirmItem.focus()
+//    }
+//    else {
+//        document.querySelector("#message").innerText = ""
+//    }
+//})
