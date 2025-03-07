1c9278a52559bdc17518fda459963c02235c3573
cchardy <pra90003@byui.edu>
2025-03-06 16:29:57 -0800

    links main json

---
+++ scripts/links.js
@@ -0,0 +1,35 @@
+const baseURL = "https://cchardy.github.io/wdd230//"
+const linksURL = "https://cchardy.github.io/wdd230/data/links.json"
+const linksContainer = document.getElementById('links');
+
+async function getLinks() {
+    const response = await fetch(linksURL)
+    if (response.ok) {
+        const data = await response.json()
+        displayLinks(data.lessons)
+    }
+}
+
+const displayLinks = (weeks) => {
+    weeks.forEach(week => {
+        let weekLi = document.createElement('li')
+        weekLi.textContent = `Week ${week.lesson} `
+        week.links.forEach(link => {
+
+            let aLink = document.createElement('a')
+
+
+            aLink.href = link.url
+            aLink.textContent = ` | ${link.title}`
+
+            weekLi.appendChild(aLink)
+            linksContainer.appendChild(weekLi)
+
+        })
+
+
+    });
+
+}
+
+getLinks();
