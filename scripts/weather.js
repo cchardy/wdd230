7442a21f43fd110aac10371b6d6d8aee59be8a62
cchardy <pra90003@byui.edu>
2025-03-10 16:02:50 -0700

    weather home page

---
+++ scripts/weather.js
@@ -0,0 +1,31 @@
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=47.78&lon=-117.47&units=imperial&appid=f91237fb3b7de2e50a66bea388132150`
+
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector('figcaption');
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url)
+        if (response.ok) {
+            const data = await response.json()
+            displayResults(data)
+        }
+        else {
+            throw Error(await response.text())
+        }
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${parseInt(data.main.temp.toFixed(0))}&deg;F`
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
+    let desc = data.weather[0].description
+    weatherIcon.setAttribute("src", iconsrc)
+    weatherIcon.setAttribute("alt", "Weather Icon")
+    captionDesc.textContent = `${desc}`
+}
+apiFetch()
