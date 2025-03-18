0f40fdc70e7462f13b012abfede7ed1b73910263
cchardy <pra90003@byui.edu>
2025-03-18 15:24:49 -0700

    weather

---
+++ chamber/scripts/weather.js
@@ -0,0 +1,38 @@
+// Weather for Chamber
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=42.94&lon=-112.84&units=imperial&appid=f91237fb3b7de2e50a66bea388132150";
+const forecast = "https://api.openweathermap.org/data/2.5/forecast?lat=42.94&lon=-112.84&units=imperial&appid=f91237fb3b7de2e50a66bea388132150";
+
+const currentTemp = document.querySelector("#temperature");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector('figcaption');
+const weatherForecast = document.querySelector("#forecast");
+
+let windSpeed = document.querySelector("#windSpeed");
+let windChill = document.querySelector("#windChill");
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url)
+        if (response.ok) {
+            const data = await response.json()
+            displayResults(data)
+        }
+        else {
+            throw Error(await response.text())
+        }
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${parseInt(data.main.temp.toFixed(0))}&deg;F`
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
+    let desc = data.weather[0].description
+    weatherForecast.innerHTML = '`${parseInt(data.list[1].main.temp.toFixed(0))}&deg;F | ${parseInt(data.list[2].main.temp.toFixed(0))}&deg;F | ${parseInt(data.list[3].main.temp.toFixed(0))}&deg;F`'
+    weatherIcon.setAttribute("src", iconsrc)
+    weatherIcon.setAttribute("alt", "Weather Icon")
+    captionDesc.textContent = `${desc}`
+}
+apiFetch()
--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,37 +1,16 @@
-// Weather for Chamber
-const url = `https://api.openweathermap.org/data/2.5/weather?lat=42.94&lon=-112.84&units=imperial&appid=f91237fb3b7de2e50a66bea388132150`
-
-let currentTemp = document.querySelector("#temperature");
-let weatherIcon = document.querySelector("#weather-icon");
-let captionDesc = document.querySelector('figcaption');
-
-let weatherForecast = document.querySelector("#forecast");
-let windSpeed = document.querySelector("#windspeed");
-let windChill = document.querySelector("#windchill");
-
-async function apiFetch() {
-    try {
-        const response = await fetch(url)
-        if (response.ok) {
-            const data = await response.json()
-            displayResults(data)
-        }
-        else {
-            throw Error(await response.text())
-        }
-    }
-    catch (error) {
-        console.log(error)
-    }
+function calculateWindChill(temperature, windSpeed) {
+    return 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
 }
 
-function displayResults(data) {
-    currentTemp.innerHTML = `${parseInt(data.main.temp.toFixed(0))}&deg;F`
-    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
-    let desc = data.weather[0].description
-    weatherForecast.innerHTML = '`${parseInt(data.list[1].main.temp.toFixed(0))}&deg;F | ${parseInt(data.list[2].main.temp.toFixed(0))}&deg;F | ${parseInt(data.list[3].main.temp.toFixed(0))}&deg;F`'
-    weatherIcon.setAttribute("src", iconsrc)
-    weatherIcon.setAttribute("alt", "Weather Icon")
-    captionDesc.textContent = `${desc}`
+function updateWindChill() {
+
+    const temperature = parseFloat(document.getElementById("temperature").value);
+    const windSpeed = parseFloat(document.getElementById("windSpeed").value);
+
+    if (temperature <= 50 && windSpeed > 3.0) {
+        const windChill = calculateWindChill(temperature, windSpeed);
+        document.getElementById("windChill").textContent = "Wind chill: " + Math.round(windChill * 100) / 100 + " °F";
+    } else {
+        document.getElementById("windChill").textContent = "Calculate wind chill above.";
+    }
 }
-apiFetch()
